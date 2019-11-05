480e3a3f77e719e3e1389c68e5a830ebb4aaa811
mbt-etienne-g <etienne.garin@mybraintech.com>
2019-11-05 12:31:29 +0100

    added classes in flavors

---
+++ app/src/full/java/com/mybraintech/testsdkk/ClassFull.java
@@ -0,0 +1,13 @@
+package com.mybraintech.testsdkk;
+
+public class ClassFull {
+
+
+    public ClassFull(){
+
+    }
+
+    public void showSomething(){
+
+    }
+}
---
+++ app/src/lite/java/com/mybraintech/testsdkk/ClassLite.java
@@ -0,0 +1,13 @@
+package com.mybraintech.testsdkk;
+
+public class ClassLite {
+
+
+    public ClassLite(){
+
+    }
+
+    public void showSomething(){
+
+    }
+}
