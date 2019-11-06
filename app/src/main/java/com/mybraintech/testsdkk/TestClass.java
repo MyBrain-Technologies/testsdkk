d555b151ad5b40cfa0fb0b3f85dac03bc138f70f
mbt-etienne-g <etienne.garin@mybraintech.com>
2019-11-06 16:40:37 +0100

    added test debug release

--- app/src/main/java/com/mybraintech/testsdkk/TestClass.java
+++ app/src/main/java/com/mybraintech/testsdkk/TestClass.java
@@ -2,6 +2,7 @@ package com.mybraintech.testsdkk;
 
 public class TestClass {
 
+    public int variable = BuildConfig.DEBUG ? 1 : 0;
 
     public void HelloWorld(){
         System.out.println("Hello worlds");
